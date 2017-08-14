d0000f40126f2443efd2accf7062ccd8d8fd35b4
aakash <aakash21696@gmail.com>
2017-08-14 21:22:39 +0530

    Fresh Start

---
+++ about.php
@@ -0,0 +1,101 @@
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>About us</title>
+
+<!-- Bootstrap Core CSS -->
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Theme CSS -->
+
+<link href="css/freelancer.min.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+
+
+</head>
+<body id="page-top" class="index">
+<div id="skipnav"><a href="#maincontent">Skip to main content</a></div>
+
+    <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="./index.php">Power Point Design Service</a>
+            </div>
+
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="./pricing.php">Pricing</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="./contact.php">Contact Us</a>
+                    </li>
+
+                    <li class="page-scroll">
+                        <a href="./about.php">About Us</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="./index.php#contact">Get Started!</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <br><br>
+    <section class="success" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>About</h2>
+                    <!-- <hr class="star-light"> -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>We are a design team focused on creating remarkable Power Point presentations for startups, entrepreneurs, individuals, students, etc . Our team has turned hundreds of presentations into stunning decks that people love. Get started today by uploading your PowerPoint presentation.</p>
+                </div>
+                <div class="col-lg-4">
+                    <p>You dream, we create. We are the rungs of the ladder that take your ideas towards success.</p>
+                </div>
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <a href="./index.php#contact" class="btn btn-lg btn-outline">
+                      Get Started!
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></script>
+</body>
---
+++ contact.php
@@ -0,0 +1,148 @@
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>About us</title>
+
+<!-- Bootstrap Core CSS -->
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Theme CSS -->
+
+<link href="css/freelancer.min.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+
+
+</head>
+<body id="page-top" class="index">
+<div id="skipnav"><a href="#maincontent">Skip to main content</a></div>
+
+    <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="./index.php">Power Point Design Service</a>
+            </div>
+
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="./pricing.php">Pricing</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="./contact.php">Contact Us</a>
+                    </li>
+
+                    <li class="page-scroll">
+                        <a href="./about.php">About Us</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="./index.php#contact">Get Started!</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <br><br>
+    <section>
+<div class="container">
+  <div class="row">
+      <div class="col-lg-12 text-center">
+          <h2>Contact Us!</h2>
+      </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <p>We are always here to help you. Reach out to us via the below form and we will get back to you ASAP! </p>
+
+        </div>
+        <br>
+
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+                <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+
+                <form action="" method="post" name="sentMessage" id="contactForm">
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label for="name">Name/Company Name</label>
+                            <input type="text" class="form-control" placeholder="Name/Company Name*" name="name" id="name" data-validation-required-message="Please enter your name." required>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label for="email">Email Address</label>
+                            <input type="email" class="form-control" placeholder="Email Address*" name="email" id="email" data-validation-required-message="Please enter your email address." required>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label for="phone">Phone Number</label>
+                            <input type="tel" class="form-control" placeholder="Phone Number" id="phone" name="mobile" required data-validation-required-message="Please enter your phone number.">
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label for="link">Your Website link</label>
+                            <input type="url" name="link" class="form-control"
+                            placeholder="Your Website Link" id="link"
+                            >
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label for="message">Message</label>
+                            <textarea rows="5" class="form-control"
+                            placeholder="Your Message"
+                            id="message" name="message"
+                            ></textarea>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+                  <div class="row">
+                        <div class="form-group col-xs-12">
+                            <button type="submit" name="submit" class="btn btn-success btn-lg">Submit!</button>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+
+      
+
+    </div>
+    </div>
+  </section>
